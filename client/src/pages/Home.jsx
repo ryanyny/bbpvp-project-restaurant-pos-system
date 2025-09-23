0bc445c0277a6b2413dc1f7879949d3e7908f614
ryanyny <ryanfebriansyah060207@gmail.com>
2025-09-23 09:40:20 +0700

    Create a home section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,13 @@ import React from "react"
 const Home = () => {
     return (
         <div>
-            Home
+            <section className="bg-[#1f1f1f] h-[calc(100vh-5rem)] overflow-hidden flex gap-3">
+                {/* Left div */}
+                <div className="flex-[3] bg-red-600"></div>
+
+                {/* Right div */}
+                <div className="flex-[2] bg-blue-600"></div>
+            </section>
         </div>
     )
 }
