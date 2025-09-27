7b67851500ff1a69c3aa47d3dd65f13f04838e74
ryanyny <ryanfebriansyah060207@gmail.com>
2025-09-27 13:04:20 +0700

    error handler setup

---
+++ server/config/config.js
@@ -0,0 +1,9 @@
+require("dotenv").config()
+
+const config = Object.freeze({
+    port: process.env.PORT || 3000,
+    databaseURI: process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/restohub",
+    nodeEnv: process.env.NODE_ENV || "development"
+})
+
+module.exports = config
--- server/server.js
+++ server/server.js
@@ -1,15 +1,19 @@
-require("dotenv").config()
 const express = require("express")
 const connectDB = require("./config/database")
+const config = require("./config/config")
+const globalErrorHandler = require("./middleware/globalErrorHandler")
+
 const app = express()
 
-const PORT = process.env.PORT
+const PORT = config.port
 connectDB()
 
 app.get("/", (req, res) => {
     res.json({ message: "Hello from Server!" })
 })
 
+app.use(globalErrorHandler)
+
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`)
+    console.log(`✅ Server is listening on port ${PORT}`)
 })
